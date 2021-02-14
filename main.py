c576005417e033790ac6ab5b4ff393a4f1aff556
Cássio <cassiobernardes96@gmail.com>
2021-02-14 17:53:21 -0300

    New functions

--- main.py
+++ main.py
@@ -17,5 +17,61 @@ class Time_conversion():
         del (list[6])
         return list
 
+    def list_into_number(self):
+        list = self.only_numbers()
+        hour = ''
+        for i in range(0, len(list)):
+            hour += list[i]
+        return hour
+
     def day_or_night(self):
-        pass
+        if 'P' in self.s:
+            hour = int(self.list_into_number())
+            military_time = hour + 120000
+            return military_time
+
+        elif self.only_numbers()[0] == '1' and self.only_numbers()[1] == '2':
+            hour = int(self.list_into_number())
+            military_time = hour - 120000
+            military_time = '000' + str(military_time)
+            return military_time
+
+
+
+""" hour = ''
+     if 'P' in self.s:
+         del(list[2])
+         del(list[4])
+         del(list[6])
+         del(list[6])
+
+         for i in range(0, len(list)):
+             hour += list[i]
+
+         list =[]
+         military_time = int(hour) + 120000
+
+         for i in range(0, len(str(military_time))):
+             list.append(str(military_time)[i])
+         list.insert(2,':')
+         list.insert(5,':')
+         hour = ''
+
+         for i in range(0, len(list)):
+             hour+= list[i]
+
+         if int(hour) == 24:
+
+
+     else:
+         del(list[8])
+         del(list[8])
+
+         hour = ''
+         for i in range(0, len(list)):
+             hour += list[i]
+
+     print(hour)"""
+
+result = Time_conversion('12:05:45AM')
+result.day_or_night()
--- test_time_conversion.py
+++ test_time_conversion.py
@@ -9,15 +9,13 @@ class TestTimeConversion(TestCase):
     def tests_if_Time_Conversion_receivies_parameter(self):
         assert result.s == '07:05:45PM'
 
-    def tests_if_day_or_night_exists(self):
-        if result.day_or_night():
-            return
-
     def tests_if_exploding_exists(self):
         if result.exploding():
             return
 
     def test_exploding_output(self):
+        hour = '07:05:45PM'
+        result = Time_conversion(hour)
         assert result.exploding() == ['0','7',':','0','5',':','4','5','P','M']
 
     def test_if_only_numbers_exist(self):
@@ -25,7 +23,32 @@ class TestTimeConversion(TestCase):
             return
 
     def test_only_numbers_output(self):
+        hour = '07:05:45PM'
+        result = Time_conversion(hour)
         assert result.only_numbers() == ['0','7','0','5','4','5']
+
+    def test_if_list_into_number_exists(self):
+        if result.list_into_number():
+            return
+
+    def tests_list_into_number_output(self):
+        hour = '07:05:45PM'
+        result = Time_conversion(hour)
+        assert result.list_into_number() == '070545'
+
+    def tests_if_day_or_night_exists(self):
+        if result.day_or_night():
+            return
+
+    def tests_day_or_night_output(self):
+        hour = '12:05:45AM'
+        result = Time_conversion(hour)
+        if 'P' in result.s:
+            assert result.day_or_night() == 190545
+        elif result.only_numbers()[0] == '1' and result.only_numbers()[1] == '2':
+            assert result.day_or_night() == '000545'
+
+
 
 hour = '07:05:45PM'
 result = Time_conversion(hour)
